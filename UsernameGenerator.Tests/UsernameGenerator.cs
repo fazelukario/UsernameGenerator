0635f66e7b931a0aca2d610aa41554a96cdb4ac8
fazelukario <fazelukario@gmail.com>
2024-04-13 22:57:08 +0300

    Added blank Tests project

---
+++ UsernameGenerator.Tests/UsernameGenerator.cs
@@ -0,0 +1,16 @@
+namespace UsernameGenrator.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
